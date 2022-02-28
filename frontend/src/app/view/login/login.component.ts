305485182caffb59a2b2221e6d713337696f214d
Cirilli Paolo <p.cirilli@campus.unimib.it>
2022-02-28 23:23:17 +0100

    Added Login and Signup View

---
+++ frontend/src/app/view/login/login.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { first } from 'rxjs';
+import { AuthenticationService } from 'src/app/auth/service/authentication.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  public loginForm!: FormGroup;
+  public loading = false;
+  public submitted = false;
+  public returnUrl: string = "";
+  public error = '';
+  public passwordTextType: boolean = false;
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _authenticationService: AuthenticationService
+  ) {
+    // redirect to home if already logged in
+    if (this._authenticationService.sessionUserValue) {
+      this._router.navigate(['/']);
+    }
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  /**
+   * Toggle password
+   */
+  togglePasswordTextType() {
+    this.passwordTextType = !this.passwordTextType;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    // Login
+    this.loading = true;
+    this._authenticationService
+      .login(this.f['email'].value, this.f['password'].value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this._router.navigate([this.returnUrl]);
+        },
+        error => {
+          this.error = error;
+          this.loading = false;
+        }
+      );
+  }
+
+  ngOnInit(): void {
+    this.loginForm = this._formBuilder.group({
+      email: ['admin@demo.com', [Validators.required, Validators.email]],
+      password: ['admin', Validators.required]
+    });
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this._route.snapshot.queryParams['returnUrl'] || '/';
+  }
+
+  ngOnDestroy(): void {
+
+  }
+}
---
+++ frontend/src/app/view/sign-up/sign-up.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { first } from 'rxjs';
+import { AuthenticationService } from 'src/app/auth/service/authentication.service';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+  public form!: FormGroup;
+  public loading = false;
+  public submitted = false;
+  public returnUrl: string = "";
+  public error = '';
+  public passwordTextType: boolean = false;
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _authenticationService: AuthenticationService
+  ) {
+    // redirect to home if already logged in
+    if (this._authenticationService.sessionUserValue) {
+      this._router.navigate(['/']);
+    }
+  }
+
+  get f() {
+    return this.form.controls;
+  }
+
+  /**
+   * Toggle password
+   */
+  togglePasswordTextType() {
+    this.passwordTextType = !this.passwordTextType;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.form.invalid) {
+      return;
+    }
+
+    // Login
+    this.loading = true;
+    this._authenticationService
+      .signUp(this.f['email'].value, this.f['firstname'].value, this.f['surname'].value, this.f['password'].value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this._router.navigate([this.returnUrl]);
+        },
+        error => {
+          this.error = error;
+          this.loading = false;
+        }
+      );
+  }
+
+  ngOnInit(): void {
+    this.form = this._formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      firstname: [null, Validators.required],
+      surname: [null, Validators.required],
+      password: [null, Validators.required]
+    });
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this._route.snapshot.queryParams['returnUrl'] || '/';
+  }
+
+  ngOnDestroy(): void {
+
+  }
+}
