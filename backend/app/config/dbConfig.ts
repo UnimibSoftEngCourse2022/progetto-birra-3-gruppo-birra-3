1c6c2734951a1b1709b3aeb018616d5b02a907de
chengjie <c.zhou1@campus.unimib.it>
2022-03-05 12:15:17 +0100

    fixed remote db security problem

--- backend/app/config/dbConfig.ts
+++ backend/app/config/dbConfig.ts
@@ -3,10 +3,18 @@ import * as dotenv from "dotenv";
 
 dotenv.config({ path: path.resolve(__dirname, "../../.env.sample") });
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME,
+  REMOTE_DB_PASSWORD,
+  REMOTE_DB_USER,
+} = process.env;
 // const url = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
 
-const url = `mongodb+srv://superuser:E7VbWRh1JmWMeqOR@clusterbirra.q0mxq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const url = `mongodb+srv://${REMOTE_DB_USER}:${REMOTE_DB_PASSWORD}@clusterbirra.q0mxq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 export { url };
 
