dabd2fa62ffbd9bdefa9e5eccf271f168526f6eb
chengjie <c.zhou1@campus.unimib.it>
2022-03-08 08:46:51 +0100

    Fetch user from findUserById

--- backend/app/controllers/userController.ts
+++ backend/app/controllers/userController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { responseHandler } from "../handler/responseHandler";
 import { tokenData } from "../helpers/jwtHelper";
+import { findUserById } from "../helpers/userHelper";
 import UserModel from "../models/userModel";
 import brewingHistoryType from "../types/brewingHistoryType";
 
@@ -8,7 +9,7 @@ class UserController {
   static getUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const tokenData: tokenData = req.token;
-      const user = await UserModel.findById(tokenData);
+      const user = await findUserById(tokenData._id);
 
       return responseHandler(res, user);
     } catch (error) {
