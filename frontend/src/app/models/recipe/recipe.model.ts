28d9f9f778528d490ad9f795cb1e3e39f0cc2817
chengjie <c.zhou1@campus.unimib.it>
2022-03-09 08:48:27 +0100

    Added getNextBatchlist

--- frontend/src/app/models/recipe/recipe.model.ts
+++ frontend/src/app/models/recipe/recipe.model.ts
@@ -1,7 +1,7 @@
 export class Recipe {
-    _id?: any;
-    title?: string;
-    color?: string;
-    description?: string;
-    published?: boolean;
+  _id?: any;
+  title?: string;
+  color?: string;
+  description?: string;
+  published?: boolean;
 }
